79f07c6af4bbaa628c73b60e90e60fdcae8ff162
tintintina <c_cereca@yahoo.com>
2016-03-27 21:02:05 -0700

    Moving dependencies to libs folder and adding error handling

--- data_transform.js
+++ data_transform.js
@@ -11,7 +11,13 @@ function base64Decode(s) {
 }
 
 function base64Encode(s) {
-	return btoa(s);
+	try {
+		return btoa(s);
+	} catch(e) {
+		if (e.name == "InvalidCharacterError") {
+			throw new FormInputException("So sorry, can't handle unicode just yet");
+		}
+	}
 }
 
 function xmlSoPretty(xmlString) {
@@ -19,8 +25,25 @@ function xmlSoPretty(xmlString) {
 }
 
 function jsonSoPretty(jsonString) {
-	var json = JSON.parse(jsonString);
-	return JSON.stringify(json, null, 4);
+	try {
+		var json = JSON.parse(jsonString);
+		return JSON.stringify(json, null, 4);
+	} catch(e) {
+		if (e instanceof SyntaxError) {
+			throw new FormInputException("Oopsie, invalid json");
+		}
+	}
+}
+function FormInputException(message) {
+	this.message = message;
+}
+function showError(section, errorMessage) {
+	$(".form-feedback", section).addClass("alert alert-danger");
+	$(".form-feedback", section).text(errorMessage);
+}
+function resetForm(section) {
+	$(".form-feedback", section).removeClass("alert alert-danger");
+	$(".form-feedback", section).empty();
 }
 $("#url-encode").submit(function(event){
 	var str = $("#url-encode-input").val();
@@ -37,9 +60,19 @@ $("#url-decode").submit(function(event){
 });
 
 $("#base64-encode").submit(function(event){
+	resetForm(this);
 	var str = $("#base64-encode-input").val();
-	var encoded = base64Encode(str);
-	$("#base64-encode-output").val(encoded);
+
+	try {
+		var encoded = base64Encode(str);
+		$("#base64-encode-output").val(encoded); 
+		$("#base64-encode .form-feedback").removeClass("alert alert-danger");
+		$("#base64-encode .form-feedback").empty();
+	} catch(e) {
+		if (e instanceof FormInputException) {
+			showError(this, e.message);
+		}
+	}
 	event.preventDefault();
 });
 
@@ -58,8 +91,21 @@ $("#xml-format").submit(function(event){
 });
 
 $("#json-format").submit(function(event){
+	resetForm(this);
 	var str = $("#json-format-input").val();
-	var prettyfied = jsonSoPretty(str);
-	$("#json-format-output").val(prettyfied);
+	try {
+		var prettyfied = jsonSoPretty(str);
+		$("#json-format-output").val(prettyfied);
+		$("#json-format .form-feedback").removeClass("alert alert-danger");
+		$("#json-format .form-feedback").empty();
+	} catch (e) {
+		if (e instanceof FormInputException) {
+			$("#json-format .form-feedback").addClass("alert alert-danger");
+			$("#json-format .form-feedback").text(e.message);
+		}
+	}
 	event.preventDefault();
+});
+$("form").on("reset", function(e) {
+	resetForm(this);
 });
